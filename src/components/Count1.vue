a3aa414959dde59e864c89be95f6b75a8c596abe
hujing <1107086473@qq.com>
2018-01-19 15:20:10 +0800

    [*]git vue_vuex_router test

---
+++ src/components/Count1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="count">
+    <p>基础用法</p>
+    <p>{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+       <p>计数器2 的值：{{ num }}</p>
+    </p>
+  </div>
+</template>
+
+<script>
+import {mapState, mapGetters, mapMutations } from 'vuex'
+  export default {
+    name: 'app',
+    computed: {
+      count() {
+        return this.$store.state.count
+      },
+      ...mapState(['num']), //mapState 辅助函数将 state 映射到计算属性中去
+    },
+    methods: {
+      increment() {
+        this.$store.dispatch('increment')
+      },
+      decrement() {
+        this.$store.commit('decrement')
+      }
+    }
+  }
+</script>
+
+
---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+const state = {
+    count: 0,
+    num:0,
+};
+const mutations = {
+    increment: state => state.count++,
+    decrement: state => state.count--,
+    ADD_ITEM: (state, num) => state.num += num,
+    SUB_ITEM: (state, num) => state.num -= num,
+};
+const actions = {
+    increment({commit}){
+        commit('increment')
+    }
+};
+const getters = {
+    num: state => state.num
+}
+const store = new Vuex.Store({
+    state,
+    getters,
+    actions,
+    mutations
+})
+
+export default store;
